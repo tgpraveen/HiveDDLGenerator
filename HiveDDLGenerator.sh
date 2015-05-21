b6cad1dc587a4a4fc9c4d1c8e441ceca821e7b79
Praveen <tgpraveen89@gmail.com>
2015-05-21 15:22:44 -0700

    Update HiveDDLGenerator.sh

--- HiveDDLGenerator.sh
+++ HiveDDLGenerator.sh
@@ -10,4 +10,12 @@ HIVECOLUMNS=`head -n1 $FILENAME|sed s/"$DELIMITER"/" STRING, "/g`
 TABLENAME=`echo $FILENAME|cut -d'.' -f1`
 
 echo CREATE TABLE $TABLENAME \(`echo $HIVECOLUMNS` STRING\)
+echo row format serde 'com.bizo.hive.serde.csv.CSVSerde'
+echo with serdeproperties \(
+echo   \"separatorChar\" = \"$DELIMITER\"
+echo  \)
+echo STORED AS TEXTFILE
+echo TBLPROPERTIES\(\"skip.header.line.count\"=\"1\"\);
 echo LOAD DATA LOCAL INPATH \'$FILENAME\' INTO TABLE $TABLENAME\;
+
+echo ""
